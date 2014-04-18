d3481283b01292282f9785690ff78cc589f28b09
Anton Lindström <lindztr@gmail.com>
2014-04-18 12:28:14 +0200

    add example for creating an app in marathonc.go

--- marathonc.go
+++ marathonc.go
@@ -10,6 +10,21 @@ func main() {
 	var host = flag.String("host", "http://localhost:8080", "Marathon host")
 	flag.Parse()
 
+	var newApp marathon.App
+
+	newApp.Cmd = "sleep 600"
+	newApp.Id = "sleeper"
+	newApp.Mem = 64.0
+	newApp.Cpus = 0.1
+	newApp.Instances = 2
+
+	msg, err := marathon.NewApp(*host, &newApp)
+
+	if err != nil {
+		fmt.Printf("err: %s\n", err)
+	}
+	fmt.Printf("Message: %s\n", msg.Message)
+
 	// Get tasks
 	tasks, err := marathon.GetTasks(*host)
 	if err != nil {
@@ -35,8 +50,11 @@ func main() {
 		fmt.Printf("  Instances: %d\n", app.Instances)
 		fmt.Printf("  Version: %s\n", app.Version)
 		fmt.Printf("  Executor: %s\n", app.Executor)
-		fmt.Printf("  Container:\n\tImage: %s\n\tOptions: %v\n", app.Container.Image, app.Container.Options)
+		if app.Container != nil {
+			fmt.Printf("  Container:\n\tImage: %s\n\tOptions: %v\n", app.Container.Image, app.Container.Options)
+		}
 		fmt.Printf("  Cpus: %f\n", app.Cpus)
 		fmt.Printf("  Memory: %f\n", app.Mem)
 	}
+
 }
