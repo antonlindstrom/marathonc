8628374d9e2c97750422e144028e8f267d6e0634
Anton Lindström <lindztr@gmail.com>
2014-04-18 12:26:38 +0200

    refactor client and add PostBody

--- marathon/client.go
+++ marathon/client.go
@@ -1,6 +1,7 @@
 package marathon
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 )
@@ -12,6 +13,26 @@ func GetBody(url string) ([]byte, error) {
 		return nil, err
 	}
 
+	// Send request to endpoint
+	return SendRequest(req)
+}
+
+// Post request to an endpoint
+func PostBody(url string, payload []byte) ([]byte, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	// Set JSON header
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send request to endpoint
+	return SendRequest(req)
+}
+
+// Send request
+func SendRequest(req *http.Request) ([]byte, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
