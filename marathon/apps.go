8fde571dc138b19bf857890b6d9e15fb0717ba3c
Anton Lindström <lindztr@gmail.com>
2014-04-18 12:27:41 +0200

    fix marathon apps and method for creating app

--- marathon/apps.go
+++ marathon/apps.go
@@ -11,27 +11,32 @@ type Apps struct {
 
 // A marathon app
 type App struct {
-	Cmd           string            `json:"cmd"`
-	Constraints   [][]string        `json:"constaints"`
-	Container     Container         `json:"container"`
-	Cpus          float32           `json:"cpus"`
-	Env           map[string]string `json:"env"`
-	Executor      string            `json:"executor"`
-	Id            string            `json:"id"`
-	Instances     int               `json:"instances"`
-	Mem           float32           `json:"mem"`
-	Ports         []int             `json:"ports"`
-	TaskRateLimit float32           `json:"tasksRateLimit"`
-	TasksRunning  int               `json:"tasksRunning"`
-	TasksStaged   int               `json:"tasksStaged"`
-	Uris          []string          `json:"uris"`
-	Version       string            `json:"version"`
+	Cmd           string            `json:"cmd,omitempty"`
+	Constraints   [][]string        `json:"constaints,omitempty"`
+	Container     *Container        `json:"container,omitempty"`
+	Cpus          float32           `json:"cpus,omitempty"`
+	Env           map[string]string `json:"env,omitempty"`
+	Executor      string            `json:"executor,omitempty"`
+	Id            string            `json:"id,omitempty"`
+	Instances     int               `json:"instances,omitempty"`
+	Mem           float32           `json:"mem,omitempty"`
+	Ports         []int             `json:"ports,omitempty"`
+	TaskRateLimit float32           `json:"tasksRateLimit,omitempty"`
+	TasksRunning  int               `json:"tasksRunning,omitempty"`
+	TasksStaged   int               `json:"tasksStaged,omitempty"`
+	Uris          []string          `json:"uris,omitempty"`
+	Version       string            `json:"version,omitempty"`
 }
 
 // A marathon app container
 type Container struct {
 	Image   string   `json:"image"`
 	Options []string `json:"options"`
+}
+
+// Message, result from post
+type Msg struct {
+	Message string `json:"message"`
 }
 
 // Get apps from endpoint
@@ -46,4 +51,29 @@ func GetApps(host string) (*Apps, error) {
 	err = json.Unmarshal(body, &apps)
 
 	return &apps, err
+}
+
+// Create new app at endpoint
+func NewApp(host string, app *App) (*Msg, error) {
+	// Unmarshal
+	b, err := json.Marshal(&app)
+	if err != nil {
+		return nil, err
+	}
+
+	// Send request
+	body, err := PostBody(host+"/v2/apps", b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal if body exists, body is empty if response
+	// is ok.
+	if string(body) != "" {
+		var msg Msg
+		err = json.Unmarshal(body, &msg)
+		return &msg, err
+	}
+
+	return &Msg{Message: "OK"}, nil
 }
